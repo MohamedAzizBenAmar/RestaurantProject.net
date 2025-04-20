8f23ce4b5bc5059803ed85e73100d91c1fdced0d
MohamedAzizBenAmar <mohamedaziz.benamar@isimsf.u-sfax.tn>
2025-04-20 14:33:08 +0100

    Recovered all work from backup

---
+++ restaurant/Controllers/IngredientController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using restaurant.Data;
+using restaurant.Models;
+
+namespace restaurant.Controllers
+{
+    public class IngredientController : Controller
+    {
+        private Repository<Ingredient> ingredients;
+        public IngredientController(ApplicationDbContext context)
+        {
+            ingredients = new Repository<Ingredient>(context);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await ingredients.GetAllAsync());
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient>() { Includes = "ProductIngredients.Product" }));
+        }
+    }
+}
