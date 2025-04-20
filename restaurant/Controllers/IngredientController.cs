e471b7a9667f77223bdbf73c4bd681e9124edf18
MohamedAzizBenAmar <mohamedaziz.benamar@isimsf.u-sfax.tn>
2025-04-20 18:26:16 +0100

    Complete Ingredient

--- restaurant/Controllers/IngredientController.cs
+++ restaurant/Controllers/IngredientController.cs
@@ -20,5 +20,53 @@ namespace restaurant.Controllers
         {
             return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient>() { Includes = "ProductIngredients.Product" }));
         }
+        //Ingeredient/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IngredientId,Name")] Ingredient ingredient)
+        {
+            if(ModelState.IsValid)
+            {
+                await ingredients.AddAsync(ingredient);
+                return RedirectToAction("Index");
+            }
+            return View(ingredient);
+
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient>() { Includes = "ProductIngredients.Product" }));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Ingredient ingredient)
+        {
+            
+              await ingredients.DeleteAsync(ingredient.IngredientId);
+              return RedirectToAction("Index");
+            
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient>() { Includes = "ProductIngredients.Product" }));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                await ingredients.UpdateAsync(ingredient);
+                return RedirectToAction("Index");
+            }
+            return View(ingredient);
+        }
     }
 }
