b0723d84cd950b15dcc079bbc25dfe93b5b8f508
MohamedAzizBenAmar <mohamedaziz.benamar@isimsf.u-sfax.tn>
2025-04-20 23:44:44 +0100

    part of product

---
+++ restaurant/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using restaurant.Data;
+using restaurant.Models;
+
+namespace restaurant.Controllers
+{
+    public class ProductController : Controller
+    {
+        private Repository<Product> products;
+        private Repository<Ingredient> ingredients;
+        private Repository<Category> categories;
+
+
+        public ProductController(ApplicationDbContext context)
+        {
+            products = new Repository<Product>(context);
+            ingredients = new Repository<Ingredient>(context);
+            categories = new Repository<Category>(context);
+
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await products.GetAllAsync());
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddEdit(int id)
+        {
+            ViewBag.Ingredients = await ingredients.GetAllAsync();
+            ViewBag.Categories = await categories.GetAllAsync();
+            if (id == 0)
+            {
+                ViewBag.Operation = "Add";
+                return View(new Product());
+            }
+            else
+            {
+                Product product = await products.GetByIdAsync(id, new QueryOptions<Product>
+                {
+                    Includes = "ProductIngredients.Ingredient, Category"
+                });
+                ViewBag.Operation = "Edit";
+                return View(product);
+            }
+        }
+
+    }
+}
