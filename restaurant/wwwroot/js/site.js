8ddb66bc856a0ac519b20a1b8af2b1d35f5f174f
MohamedAzizBenAmar <mohamedaziz.benamar@isimsf.u-sfax.tn>
2025-04-24 02:20:00 +0100

    design fixed separe admin user

--- restaurant/wwwroot/js/site.js
+++ restaurant/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    // Smooth scroll for anchor links, excluding dropdowns
+    $('a.nav-link').on('click', function (event) {
+        if (this.hash !== "" && !$(this).is('[data-bs-toggle="dropdown"]')) {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800);
+        }
+    });
+});
